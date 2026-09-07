8dcc5e26c0484c7c86184e19ba71e55b12cbc2f7
ExpertDaniil <expertdaniil062006@gmail.com>
2026-09-07 14:16:17 +0300

    feat(scaffold): export cyber ACI provider

--- agent/scaffold/__init__.py
+++ agent/scaffold/__init__.py
@@ -1,5 +1,6 @@
 """Experimental extensible scaffold for the autonomous cybersecurity agent."""
 
+from .aci import CyberACIProvider
 from .bootstrap import ScaffoldApplication, build_default_application
 from .contracts import CapabilityLevel, KernelLimits, PlanDecision, PlanStrategy, ToolSpec
 from .extensions import ScaffoldExtension, gdb_extension
@@ -11,6 +12,7 @@ __all__ = [
     "AgentKernel",
     "AgentState",
     "CapabilityLevel",
+    "CyberACIProvider",
     "KernelLimits",
     "PlanDecision",
     "PlanStrategy",
